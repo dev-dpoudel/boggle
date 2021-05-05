82842f2c7029d1beadd72aac930a08769b46e634
dev-dpoudel <diensh2019@cdcsit.edu.np>
2021-05-05 08:12:05 +0545

    Removed Debug messages.

--- tests/test_game.py
+++ tests/test_game.py
@@ -3,8 +3,10 @@ import requests
 
 
 class TestBoggle(unittest.TestCase):
-    
+
     baseurl = "http://localhost:8000/{}"
+
+    headers = {'cookie': 'session=.eJxNj7EKwzAMRP_Fc76gmwgBbQILaouSIa2XLA2khVJK_73SOUOHk4V9ejp_0nVb9pZOl2RpSOQa_86zq7rUlV3FxS5x2fE2wT8Pqa2357rdl_0dOOEYKhxWyYBRVCVDjd4oYGPGDQcoU0wxPCPFSpYJBEJv8CAqmCqIRxVTiJIVtPBnI_TS0bVDFTzF_thAykga3oIsPBVYkeuI11eDRNZDCqw1_v7a9vbwb8_fH62oTdk.YJH2-A.qu5BaGdRLezjtQLV6j2po9IqHVU'}
 
     def test_board(self):
         url = self.baseurl.format("keys")
@@ -16,16 +18,15 @@ class TestBoggle(unittest.TestCase):
 
     def test_isValid_word(self):
         url = self.baseurl.format("list")
-        response = requests.get(url)
+        response = requests.get(url, headers=self.headers)
         data = response.json()
         words = data["words"]
         # Test the size of the Board. It must be 4 * 4
-        self.assertEquals(len(words), 0, "No words found")
-        print(words, "Test")
+        self.assertNotEqual(len(words), 0, "No words found")
         # Test Word is valid
         query = "valid?word=%s" % (words[0])
         url = self.baseurl.format(query)
-        response = requests.get(url)
+        response = requests.put(url, headers=self.headers)
         data = response.json()
         success = data["success"]
         # Check for Success
@@ -35,28 +36,27 @@ class TestBoggle(unittest.TestCase):
         # Test Word is In valid
         query = "/valid?word=%s" % ("RoughExampleTesting")
         url = self.baseurl.format(query)
-        response = requests.get(url)
+        response = requests.put(url, headers=self.headers)
         data = response.json()
         success = data["success"]
         # Check for Success
-        self.assertTrue(success, "Word is found")
+        self.assertFalse(success, "Word is found")
 
     def test_get_score(self):
-        query = "valid?word=%s" % ("RoughExampleTesting")
+        query = "results"
         url = self.baseurl.format(query)
-        response = requests.get(url)
+        response = requests.get(url, headers=self.headers)
         data = response.json()
-        success = data["success"]
-        # Check for Success
-        self.assertTrue(success, response.text)
+        results = data["results"]
+        assert results is not None
 
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(TestBoggle('test_board'))
     suite.addTest(TestBoggle('test_isValid_word'))
-    # suite.addTest(TestBoggle('test_inValid_word'))
-    # suite.addTest(TestBoggle('test_get_score'))
+    suite.addTest(TestBoggle('test_inValid_word'))
+    suite.addTest(TestBoggle('test_get_score'))
     return suite
 
 
